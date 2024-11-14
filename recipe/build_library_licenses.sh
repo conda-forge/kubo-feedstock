3f41fb121be57ea96452c23991e3205256499b19
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-11-14 10:07:53 -0600

    whitespace, don't expect the license log

--- recipe/build_library_licenses.sh
+++ recipe/build_library_licenses.sh
@@ -17,60 +17,60 @@ export LIBRARY_LICENSES_PATH="${SRC_DIR}/library_licenses/"
 
 go-licenses save \
     "." \
-    --ignore archive/tar  \
-    --ignore archive/zip  \
-    --ignore bufio  \
-    --ignore bytes  \
-    --ignore cmp  \
+    --ignore archive/tar \
+    --ignore archive/zip \
+    --ignore bufio \
+    --ignore bytes \
+    --ignore cmp \
     --ignore compress/flate \
-    --ignore compress/gzip  \
-    --ignore compress/zlib  \
-    --ignore container/heap  \
-    --ignore container/list  \
-    --ignore context  \
-    --ignore crypto  \
-    --ignore crypto/aes  \
-    --ignore crypto/cipher  \
-    --ignore crypto/ecdh  \
-    --ignore crypto/ecdsa  \
-    --ignore crypto/ed25519  \
-    --ignore crypto/elliptic  \
-    --ignore crypto/hmac  \
-    --ignore crypto/md5  \
-    --ignore crypto/rand  \
-    --ignore crypto/rsa  \
-    --ignore crypto/sha1  \
-    --ignore crypto/sha256  \
-    --ignore crypto/sha512  \
-    --ignore crypto/subtle  \
-    --ignore crypto/tls  \
-    --ignore crypto/x509  \
-    --ignore crypto/x509/pkix  \
-    --ignore database/sql  \
-    --ignore database/sql/driver  \
-    --ignore embed  \
-    --ignore encoding  \
-    --ignore encoding/asn1  \
-    --ignore encoding/base32  \
-    --ignore encoding/base64  \
-    --ignore encoding/binary  \
-    --ignore encoding/gob  \
-    --ignore encoding/hex  \
-    --ignore encoding/json  \
-    --ignore encoding/pem  \
-    --ignore encoding/xml  \
-    --ignore errors  \
-    --ignore expvar  \
-    --ignore flag  \
-    --ignore fmt  \
+    --ignore compress/gzip \
+    --ignore compress/zlib \
+    --ignore container/heap \
+    --ignore container/list \
+    --ignore context \
+    --ignore crypto \
+    --ignore crypto/aes \
+    --ignore crypto/cipher \
+    --ignore crypto/ecdh \
+    --ignore crypto/ecdsa \
+    --ignore crypto/ed25519 \
+    --ignore crypto/elliptic \
+    --ignore crypto/hmac \
+    --ignore crypto/md5 \
+    --ignore crypto/rand \
+    --ignore crypto/rsa \
+    --ignore crypto/sha1 \
+    --ignore crypto/sha256 \
+    --ignore crypto/sha512 \
+    --ignore crypto/subtle \
+    --ignore crypto/tls \
+    --ignore crypto/x509 \
+    --ignore crypto/x509/pkix \
+    --ignore database/sql \
+    --ignore database/sql/driver \
+    --ignore embed \
+    --ignore encoding \
+    --ignore encoding/asn1 \
+    --ignore encoding/base32 \
+    --ignore encoding/base64 \
+    --ignore encoding/binary \
+    --ignore encoding/gob \
+    --ignore encoding/hex \
+    --ignore encoding/json \
+    --ignore encoding/pem \
+    --ignore encoding/xml \
+    --ignore errors \
+    --ignore expvar \
+    --ignore flag \
+    --ignore fmt \
     --ignore go/ast \
-    --ignore go/format  \
+    --ignore go/format \
     --ignore go/parser \
     --ignore go/printer \
-    --ignore go/token  \
-    --ignore hash  \
-    --ignore hash/crc32  \
-    --ignore hash/fnv  \
+    --ignore go/token \
+    --ignore hash \
+    --ignore hash/crc32 \
+    --ignore hash/fnv \
     --ignore internal/abi \
     --ignore internal/asan \
     --ignore internal/bytealg \
@@ -105,53 +105,53 @@ go-licenses save \
     --ignore internal/sysinfo \
     --ignore internal/testlog \
     --ignore internal/unsafeheader \
-    --ignore io  \
-    --ignore io/fs  \
-    --ignore io/ioutil  \
+    --ignore io \
+    --ignore io/fs \
+    --ignore io/ioutil \
     --ignore iter \
-    --ignore log  \
-    --ignore log/slog  \
+    --ignore log \
+    --ignore log/slog \
     --ignore maps \
-    --ignore math  \
-    --ignore math/big  \
-    --ignore math/bits  \
-    --ignore math/cmplx  \
-    --ignore math/rand  \
-    --ignore math/rand/v2  \
-    --ignore mime  \
-    --ignore mime/multipart  \
-    --ignore net  \
-    --ignore net/http  \
-    --ignore net/http/httptest  \
-    --ignore net/http/httptrace  \
-    --ignore net/netip  \
-    --ignore net/textproto  \
-    --ignore net/url  \
-    --ignore os  \
+    --ignore math \
+    --ignore math/big \
+    --ignore math/bits \
+    --ignore math/cmplx \
+    --ignore math/rand \
+    --ignore math/rand/v2 \
+    --ignore mime \
+    --ignore mime/multipart \
+    --ignore net \
+    --ignore net/http \
+    --ignore net/http/httptest \
+    --ignore net/http/httptrace \
+    --ignore net/netip \
+    --ignore net/textproto \
+    --ignore net/url \
+    --ignore os \
     --ignore os/exec \
-    --ignore os/signal  \
-    --ignore path  \
-    --ignore path/filepath  \
-    --ignore reflect  \
-    --ignore regexp  \
-    --ignore runtime  \
-    --ignore runtime/debug  \
-    --ignore runtime/metrics  \
-    --ignore runtime/pprof  \
-    --ignore slices  \
-    --ignore sort  \
-    --ignore strconv  \
-    --ignore strings  \
-    --ignore sync  \
-    --ignore sync/atomic  \
-    --ignore syscall  \
-    --ignore testing  \
+    --ignore os/signal \
+    --ignore path \
+    --ignore path/filepath \
+    --ignore reflect \
+    --ignore regexp \
+    --ignore runtime \
+    --ignore runtime/debug \
+    --ignore runtime/metrics \
+    --ignore runtime/pprof \
+    --ignore slices \
+    --ignore sort \
+    --ignore strconv \
+    --ignore strings \
+    --ignore sync \
+    --ignore sync/atomic \
+    --ignore syscall \
+    --ignore testing \
     --ignore text/tabwriter \
-    --ignore text/template  \
-    --ignore time  \
-    --ignore unicode  \
-    --ignore unicode/utf16  \
-    --ignore unicode/utf8  \
+    --ignore text/template \
+    --ignore time \
+    --ignore unicode \
+    --ignore unicode/utf16 \
+    --ignore unicode/utf8 \
     --ignore unique \
     --ignore vendor/golang.org/x/crypto/chacha20poly1305 \
     --ignore vendor/golang.org/x/crypto/cryptobyte \
