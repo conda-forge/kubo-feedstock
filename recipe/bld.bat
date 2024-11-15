303981979c52d77a224ec3167f808eb3ffe66bd1
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-11-14 20:07:30 -0600

    use go-cgo everywhere

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,9 @@
-copy "%RECIPE_DIR%\build_win.sh" .
-if %errorlevel% neq 0 exit /b %errorlevel%
+copy "%RECIPE_DIR%\build_win.sh" . || exit 1
 
-set PREFIX=%PREFIX:\=/%
-set SRC_DIR=%SRC_DIR:\=/%
-set MSYSTEM=MINGW%ARCH%
+set "PREFIX=%PREFIX:\=/%"
+set "SRC_DIR=%SRC_DIR:\=/%"
+set "MSYSTEM=MINGW%ARCH%"
 set MSYS2_PATH_TYPE=inherit
 set CHERE_INVOKING=1
 
-bash -lc "./build_win.sh"
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-exit /b 0
+bash -lc "./build_win.sh" || exit 1
