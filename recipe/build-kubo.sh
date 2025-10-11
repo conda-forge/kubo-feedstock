8cbba09ff8e23d9c8f03eea36336e7273705b571
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-10-11 15:45:42 -0500

    try without cgo altogether

--- recipe/build-kubo.sh
+++ recipe/build-kubo.sh
@@ -5,12 +5,6 @@ GOPATH="$( pwd )"
 export GOPATH
 export GOROOT="${BUILD_PREFIX}/go"
 
-export CGO_ENABLED=1
-export CGO_CFLAGS="${CFLAGS}"
-export CGO_LDFLAGS="${LDFLAGS}"
-export GOFLAGS="-buildmode=pie -trimpath -ldflags=-linkmode=external -mod=vendor -modcacherw"
-export GOTAGS="openssl nofuse"
-
 cd "src/github.com/ipfs/kubo"
 
 go build \
